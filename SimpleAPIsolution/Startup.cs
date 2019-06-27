59538be63a53482d67d997ba094a5cf9f11aa9ac
sudscode <52221165+sudscode@users.noreply.github.com>
2019-06-27 15:43:38 +1200

    Update Startup.cs

--- SimpleAPIsolution/Startup.cs
+++ SimpleAPIsolution/Startup.cs
@@ -27,7 +27,7 @@ namespace SimpleAPIsolution
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World!////");
             });
         }
     }
